0159eff4ce5d116cdb0b9931116b0d3658d05641
nainghtetlinn <naing.work.it@gmail.com>
2026-03-10 15:12:28 +0630

    feat: reusable textbox component

---
+++ src/components/ui/textbox/index.tsx
@@ -0,0 +1,90 @@
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cloneElement, isValidElement, ReactElement, ReactNode } from "react";
+import {
+  TextInput,
+  TextInputProps,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from "react-native";
+
+const textboxVariants = cva("flex-row items-center rounded-lg gap-1.5", {
+  variants: {
+    variant: {
+      outline: "bg-natural-white border-yellow-700 border",
+      contained: "bg-grey-200",
+    },
+    size: {
+      sm: "px-4 py-2",
+      md: "px-4 py-3",
+    },
+  },
+  defaultVariants: {
+    variant: "outline",
+    size: "md",
+  },
+});
+
+const iconSizeVariants = {
+  sm: 16,
+  md: 20,
+};
+
+export type TextboxProps = {
+  leftIcon?: ReactNode;
+  leftIconButtonProps?: TouchableOpacityProps;
+  rightIcon?: ReactNode;
+  rightIconButtonProps?: TouchableOpacityProps;
+  disabled?: boolean;
+} & TextInputProps &
+  VariantProps<typeof textboxVariants>;
+
+export const Textbox = ({
+  variant,
+  size,
+  leftIcon,
+  leftIconButtonProps,
+  rightIcon,
+  rightIconButtonProps,
+  disabled,
+  className,
+  ...props
+}: TextboxProps) => {
+  const lIcon =
+    leftIcon && isValidElement(leftIcon)
+      ? cloneElement(
+          leftIcon as ReactElement<{ color: string; size: number }>,
+          {
+            color: "#171007",
+            size: iconSizeVariants[size ?? "md"],
+          },
+        )
+      : leftIcon;
+  const rIcon =
+    rightIcon && isValidElement(rightIcon)
+      ? cloneElement(
+          rightIcon as ReactElement<{ color: string; size: number }>,
+          {
+            color: "#171007",
+            size: iconSizeVariants[size ?? "md"],
+          },
+        )
+      : rightIcon;
+
+  return (
+    <View className={cn(textboxVariants({ variant, size }), className)}>
+      {lIcon ? (
+        <TouchableOpacity {...leftIconButtonProps}>{lIcon}</TouchableOpacity>
+      ) : null}
+      <TextInput
+        placeholderTextColor="#939393"
+        className="flex-1 text-base font-medium text-natural-black p-0 m-0"
+        {...props}
+      />
+      {rIcon ? (
+        <TouchableOpacity {...rightIconButtonProps}>{rIcon}</TouchableOpacity>
+      ) : null}
+    </View>
+  );
+};
