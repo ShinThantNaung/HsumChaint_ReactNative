ae48160c3bbef204a90a63e9425a6c964d5ca8b0
nainghtetlinn <naing.work.it@gmail.com>
2026-03-11 14:49:25 +0630

    feat: reusable form input component & its showcase screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -18,7 +18,13 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack initialRouteName="login">
+        <Stack.Screen
+          name="login"
+          options={{
+            headerShown: false,
+          }}
+        />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen
           name="modal"
